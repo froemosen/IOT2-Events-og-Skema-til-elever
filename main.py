6b1149efe3f1fe647fb1196816372d3ee8bcecf7
froemosen <kristianantonhedegaard@gmail.com>
2022-02-01 14:41:46 +0100

    donezo

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import json
 def getTime(currentTime): #Daglig send af skema og begivenheder
     global latestTime
     
-    if currentTime.tm_hour == 8 and currentTime.tm_hour != latestTime:
+    if currentTime.tm_hour == 7 and currentTime.tm_hour != latestTime: #Tal i første del er klokkeslæt hvor skema sendes
         latestTime = currentTime.tm_hour
         return True
     else: 
@@ -30,13 +30,14 @@ def getCalendar(): #Skaffer kalender fra delt outlook-kalender
         data = f.readlines()
     
     for line in data:
-        if "Lokale:" in line:
-            room = ", "+line.removeprefix("DESCRIPTION:").partition("-")[0]
+        try:
+            if "Lokale:" in line:
+                room = ", "+line.removeprefix("DESCRIPTION:").partition("-")[0]
+        except: pass
         
         if "SUMMARY" in line: 
             try: #Prøv at tilføje lokale
                 message = line.removeprefix("SUMMARY:").replace("\n", "")
-                message += room
                 if len(room)<15: message += room
                 messageFound = True
             except: #Hvis ikke lokale er fastsat 
@@ -74,43 +75,37 @@ def getCalendar(): #Skaffer kalender fra delt outlook-kalender
 def getChanges(currentCalendar, prevCalendar): #Tjekker efter ændringer i kalender
     if currentCalendar != prevCalendar:
         print("Changes have happened")
-        newChanges = "Some change"
-        
-        return True, newChanges
-    
+        return True
     else:
-        return False, 0
+        return False
 
 
 def sendCalendar(Calendar): #Sender daglig kalender til IFTTT
     print("Sending Calendar...\n")
-    print(Calendar)
-    toSend = json.dumps({'data':Calendar.replace('"','\'')})
-    print("\n\n\n"+toSend+"\n\n\n")
-    r = requests.post('https://maker.ifttt.com/trigger/fest/json/with/key/bEOACrKudUx-DNoZ2icG0J', json={' ':"<br>"+Calendar.replace('"','\'').replace("\n", "<br>")}) #date=toSend
-    print(f"Response: {r}: {r.text}")
-
+    print(Calendar) #Normalt format
+    print("\n\n\n"+"{"+' '+":"+"<br>"+Calendar.replace('"','\'').replace("\n", "<br>")+"}"+"\n\n\n") #JSON-format
+    r = requests.post('https://maker.ifttt.com/trigger/fest/json/with/key/bEOACrKudUx-DNoZ2icG0J', json={' ':"<br>"+Calendar.replace('"','\'').replace("\n", "<br>")}) #besked sendes til ifttt
+    print(f"Response: {r}: {r.text}") #200 hvis modtaget
+    
 
 def timeToClear(unix):
     global lastClearTime
-    
-    if unix > lastClearTime+86400: 
+    if unix > lastClearTime+86400: #24 timer 
         lastClearTime = unix
         return True
     else: 
         return False
     
 
-
 if __name__ == '__main__':
     prevCalendar = getCalendar()
     latestTime = 0
-    lastClearTime = int(time.time())
-        
+    lastClearTime = int(time.time()) 
     
     while True:
         print(f"Current time: {time.ctime(int(time.time()))}")
-        currentCalendar = getCalendar() #Ny kalender hentes ind.
+        try: currentCalendar = getCalendar() #Ny kalender hentes ind.
+        except: print("NETWORK CONNECTION FAILED - Waiting another minute")
                
         #Hvis det er tid til daglige events
         if getTime(time.localtime(int(time.time()))): #Klokken er lige blevet 7 - send besked
@@ -121,14 +116,15 @@ if __name__ == '__main__':
         
         
         #Hvis der er sket ændringer siden sidste tjek
-        changeStatus, changes = getChanges(currentCalendar, prevCalendar)
+        changeStatus = getChanges(currentCalendar, prevCalendar)
             
         if changeStatus:
-            pass
+            print("Sending new changes...")
+            sendCalendar("**!!!CHANGES HAVE OCCURED IN TODAY'S PROGRAM!!!** \n"+currentCalendar)
         else:
-            print("No changes comitted")    
+            print("No changes comitted")   
             
         prevCalendar = currentCalendar
         print("\n")
-        if timeToClear(int(time.time())): os.system("cls")
+        if timeToClear(int(time.time())): os.system("cls") #Der cleares én gang per døgn for at undgå spildt RAM.
         time.sleep(60) #Opdater ændringer hvert minut
